def0b3dc14cf6b28bb8842c5982bdf02df917a9b
gwang1234 <qoi11@naver.com>
2024-03-26 10:46:55 +0900

    test

---
+++ src/main/java/com/example/bangbang_gotgot/BangbangGotgotApplication.java
@@ -0,0 +1,13 @@
+package com.example.bangbang_gotgot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangbangGotgotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BangbangGotgotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bangbang_gotgot/BangbangGotgotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bangbang_gotgot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BangbangGotgotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
