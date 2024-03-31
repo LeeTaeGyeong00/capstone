1ed9d24ce2468e340bea7a9a0e1ad4ad8522e299
LeeTaeSuk <twohugerock@gmail.com>
2024-03-31 17:20:06 +0900

    03/31 application 파일 추가 후 파일 업로드

--- src/main/java/com/example/bangbang_gotgot/controller/MainController.java
+++ src/main/java/com/example/bangbang_gotgot/controller/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-
+//2024.03.31 이태석
     @GetMapping("/")
     public String hello(){
         return "index";
