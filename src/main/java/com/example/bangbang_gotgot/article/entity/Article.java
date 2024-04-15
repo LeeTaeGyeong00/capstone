bfc8966dae1cc60b42751ed6adfb5643ed26933e
LeeTaeSuk <twohugerock@gmail.com>
2024-04-16 03:47:11 +0900

    0415 게시판 작업 완료

--- src/main/java/com/example/bangbang_gotgot/article/entity/Article.java
+++ src/main/java/com/example/bangbang_gotgot/article/entity/Article.java
@@ -1,4 +1,45 @@
 package com.example.bangbang_gotgot.article.entity;
 
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Data
+@Getter
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="articleId", updatable = false)
+    private Long id;
+
+    @Column(name="title", nullable = false)
+    private String title;
+
+    @Column(name="content", nullable = false)
+    private String content;
+
+    @CreationTimestamp
+    @Column(name="regDate", updatable = false)
+    private LocalDateTime regDate;
+
+    @LastModifiedDate
+    @Column(name="modDate")
+    private LocalDateTime modDate;
+
+    @Column(name="writer")
+    private String writer;
+
+    @Builder
+    public Article(String title, String content, String writer){
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+    }
 }
--- src/main/java/com/example/bangbang_gotgot/article/repository/ArticleRepository.java
+++ src/main/java/com/example/bangbang_gotgot/article/repository/ArticleRepository.java
@@ -1,4 +1,10 @@
 package com.example.bangbang_gotgot.article.repository;
 
-public interface ArticleRepository {
+import com.example.bangbang_gotgot.article.entity.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+    Page<Article> findByTitleContaining(String searchKeyword, Pageable pageable);
 }
